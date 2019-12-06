726cd9d287b965d3ad79364ba752a97d7d6595a0
Atangana57 <olivieratangana57@gmail.com>
2019-12-06 11:49:42 +0100

    creation dela classe magasin.h

---
+++ Magasin.hpp
@@ -0,0 +1,16 @@
+#ifndef MAGASIN_H
+#define MAGASIN_H
+#include <string>
+using namespace std;
+ 
+ class Magasin
+ {
+ public:
+ 	Magasin();
+ 	~Magasin();
+ private:
+ 	produit produit1; 
+ 	Client client1;
+ 	commande commande2;
+ 
+ };
