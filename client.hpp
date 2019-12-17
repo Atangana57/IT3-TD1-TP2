46c6f52af97dd4ff4c939d47ca32e3ffd7b3a273
Atangana57 <olivieratangana57@gmail.com>
2019-12-17 08:41:01 +0100

    creation de la classe client

---
+++ client.hpp
@@ -0,0 +1,17 @@
+#ifndef	Produits_H
+#define Produits_H
+#include <string>
+using namespace std;
+
+class Produits
+{
+public:
+	Produits();
+	~Produits();
+	string::
+private:
+	string prenom_client;
+	string nom_client;
+	string commande;
+	
+};
