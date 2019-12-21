ec71c4fd4abc3fbdddc2a55694d373adb3d18716
Atangana57 <olivieratangana57@gmail.com>
2019-12-21 14:15:39 +0100

    creation de la classe Client, question 4.a

---
+++ client.h
@@ -0,0 +1,77 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include "Produit.h"
+using namespace std;
+
+class Client
+{
+    public:
+        /** Default constructor */
+        Client(unsigned int ID,string prm,string nm,vector<Produit> pnr )
+        {
+        id=ID;
+        nom=nm;
+        prenom=prm;
+        panier=pnr;
+        }
+
+
+
+        Client(){}
+
+        virtual~Client()
+        {
+        //dtor
+        }
+
+        /** Access id
+         * \return The current value of id
+         */
+        int Getid() { return id; }
+        /** Set id
+         * \param val New value to set
+         */
+        void Setid(unsigned int val) { id = val; }
+
+
+        /** Access prenom
+         * \return The current value of prenom
+         */
+        string Getprenom() { return prenom; }
+        /** Set prenom
+         * \param val New value to set
+         */
+        void Setprenom(string val) { prenom = val; }
+        /** Access nom
+         * \return The current value of nom
+         */
+        string Getnom() { return nom; }
+        /** Set nom
+         * \param val New value to set
+         */
+        void Setnom(string val) { nom = val; }
+        /** Access panier
+         * \return The current value of panier
+         */
+        vector<Produit> Getpanier() { return panier; }
+        /** Set panier
+         * \param val New value to set
+         */
+        void Setpanier(vector<Produit> val) { panier = val; }
+
+
+
+    protected:
+
+    private:
+        unsigned int id; //!< Member variable "id"
+        string prenom; //!< Member variable "prenom"
+        string nom; //!< Member variable "nom"
+        vector<Produit> panier; //!< Member variable "panier"
+};
+
+#endif // CLIENT_H
