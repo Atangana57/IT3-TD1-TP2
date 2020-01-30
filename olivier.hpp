be39067d7116c5e8dfa5eda5d03be49a88d74073
Atangana57 <olivieratangana57@gmail.com>
2020-01-30 23:46:14 +0100

    makefile

---
+++ olivier.hpp
@@ -0,0 +1,13 @@
+//
+//  olivier.hpp
+//  
+//
+//  Created by Abbe Olivier Atangana on 30/01/2020.
+//
+
+#ifndef olivier_hpp
+#define olivier_hpp
+
+#include <stdio.h>
+
+#endif /* olivier_hpp */
---
+++ test2/test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by Abbe Olivier Atangana on 30/01/2020.
+//  Copyright © 2020 Abbe Olivier Atangana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
